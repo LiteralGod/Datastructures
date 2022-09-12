70851e416da1cf9b1b46e5401dc17dd79f3a2e77
LiteralGod <tchri20@student.sdu.dk>
2022-09-12 21:38:08 +0200

    first exercises of datastructures

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,31 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println(sum(5));
+
+        System.out.println(evenSquares(10));
+
+    }
+
+    public static int sum(int n){
+
+        if (n <= 1) {
+            return n;
+        }
+        return n + sum(n-1);
+    }
+
+
+
+    public static int evenSquares(int n){
+
+        if (n<=1){
+            return n;
+        }
+        else if (n % 2 != 0){
+            evenSquares(n-1);
+        }
+        return n*n + evenSquares(n-2);
     }
 }
